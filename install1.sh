e63796ecad20ff2cd319bbab3286f1cbd2c3294b
Trent008 <trenthekman68@gmail.com>
2024-04-23 18:00:16 -0500

    update

--- install1.sh
+++ install1.sh
@@ -1,7 +1,8 @@
-iwctl station wlan0 connect "Dordt-Student"
-pacman -Syy
-pacman -S reflector
-reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel linux linux-firmware nano adapta-gtk-theme blueberry brightnessctl code dunst efibootmgr feh firefox grub gst-plugin-pipewire intel-ucode libxft libxinerama networkmanager papirus-icon-theme picom pipewire pipewire-alsa pipewire-jack pipewire-pulse playerctl shotgun spotify-launcher terminator thunar ttf-nerd-fonts-symbols-mono xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
+# iwctl station wlan0 connect "Dordt-Student"
+mkfs.ext4 /dev/nvme0n1p2
+mkfs.fat -F 32 /dev/nvme0n1p1
+mount /dev/nvme0n1p2 /mnt
+mount --mkdir /dev/nvme0n1p1 /mnt/boot
+pacstrap /mnt base base-devel linux linux-firmware nano adapta-gtk-theme blueberry brightnessctl code dunst efibootmgr feh firefox git grub gst-plugin-pipewire intel-ucode libxft libxinerama networkmanager papirus-icon-theme picom pipewire pipewire-alsa pipewire-jack pipewire-pulse playerctl shotgun spotify-launcher terminator thunar ttf-nerd-fonts-symbols-mono xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
