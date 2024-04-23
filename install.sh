2c46d25b2655ae5721a83dc46abae030d7af12f3
Trent008 <trenthekman68@gmail.com>
2024-04-23 13:18:31 -0500

    add script to apply changes to dwm and update ultimate system config

--- install.sh
+++ install.sh
@@ -9,23 +9,9 @@ yay -S xcursor-breeze papirus-icon-theme bluetuith
 # start bluetooth and wifi
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now NetworkManager.service
-# create .xinitrc to start dwm and display batery and date
-cd ~
-sudo cp ultimate-system/.xinitrc ~
-# setup system theming
-sudo cp ultimate-system/settings.ini /etc/gtk-3.0
-mkdir .config/gtk-3.0
-mkdir .icons .icons/default
-cp ultimate-system/settings.ini .config/gtk-3.0
-cp ultimate-system/index.theme .icons/default
-cp ultimate-system/.Xresources ~
-cp ultimate-system/.gtkrc-2.0 ~
 # clone and install dmenu and dwm
 git clone https://aur.archlinux.org/yay.git
 git clone https://git.suckless.org/dmenu
 git clone https://github.com/Trent008/dwm
-cd dmenu
-sudo make clean install
-cd dwm
-sudo make clean install
-cd ~
+# compiles dwm and dmenu, creates .xinitrc, and applies themes
+./ultimate-system/update.sh
