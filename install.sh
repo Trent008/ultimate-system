942d2c4566bae81e3444666bdc2b3982a86c7639
Trent008 <trenthekman68@gmail.com>
2024-05-03 09:56:12 -0500

    start working on system install script

--- install.sh
+++ install.sh
@@ -1,21 +1,5 @@
 # install all my packages
-sudo pacman -Syu
-sudo pacman -S libxinerama libx11 libxft xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi
-sudo pacman -S code terminator thunar feh spotify-launcher playerctl firefox dunst blueberry xarchiver unzip
-sudo pacman -S adapta-gtk-theme papirus-icon-theme ttf-nerd-fonts-symbols-mono shotgun brightnessctl picom
-cd ~
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-yay -S xcursor-breeze
-# start bluetooth and wifi
-sudo systemctl enable --now bluetooth.service
-sudo systemctl enable --now NetworkManager.service
-cd ~
-# clone and install dwm
-git clone https://github.com/Trent008/dwm
-# compiles dwm and dmenu, creates .xinitrc, and applies themes
-./ultimate-system/update.sh
-# setup with my git username and email
-git config --global user.email "trenthekman68@gmail.com"
-git config --global user.name "Trent008"
+pacman -Syu
+pacstrap -K base base-devel linux linux-firmware libxinerama libx11 libxft xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi\
+> code terminator thunar feh spotify-launcher playerctl firefox dunst blueberry xarchiver unzip \
+> adapta-gtk-theme papirus-icon-theme ttf-nerd-fonts-symbols-mono shotgun brightnessctl picom
