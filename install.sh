db4cb718da1d7ad2ed6b471a3b1a8ac59b7dd718
Trent008 <trenthekman68@gmail.com>
2024-05-03 17:32:07 -0500

    revert to the simple past

--- install.sh
+++ install.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 set -x
-pacstrap -K /mnt base base-devel linux linux-firmware git sof-firmware libxinerama libx11 libxft libpulse xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi \
-networkmanager code terminator thunar feh spotify-launcher playerctl firefox dunst blueberry xarchiver unzip efibootmgr grub \
-adapta-gtk-theme papirus-icon-theme ttf-nerd-fonts-symbols-mono shotgun brightnessctl picom intel-ucode pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire
+sudo pacman -S adapta-gtk-theme \
+base-devel \
+blueberry \
+brightnessctl \
+code \
+dunst \
+feh \
+firefox \
+htop \
+inkscape \
+libxft \
+libxinerama \
+nano \
+neofetch \
+papirus-icon-theme \
+picom \
+playerctl \
+rofi \
+shotgun \
+spotify-launcher \
+terminator \
+thunar \
+ttf-nerd-fonts-symbols-mono \
+unzip \
+xarchiver \
+xorg-server \
+xorg-xinit \
+xorg-xrandr \
+xorg-xsetroot
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S xcursor-breeze
+# start bluetooth and wifi
+sudo systemctl enable --now bluetooth.service
+sudo systemctl enable --now NetworkManager.service
+cd ~
+# clone and install dwm
+git clone https://github.com/Trent008/dwm
+# compiles dwm and dmenu, creates .xinitrc, and applies themes
+./ultimate-system/update.sh
+# setup with my git username and email
+git config --global user.email "trenthekman68@gmail.com"
+git config --global user.name "Trent008"
