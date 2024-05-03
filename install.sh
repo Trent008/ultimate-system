ff8a7cfc315e53cd42641b77e541b9c00d8fed27
Trent008 <trenthekman68@gmail.com>
2024-05-03 17:38:30 -0500

    unify install script

--- install.sh
+++ install.sh
@@ -40,8 +40,21 @@ sudo systemctl enable --now NetworkManager.service
 cd ~
 # clone and install dwm
 git clone https://github.com/Trent008/dwm
-# compiles dwm and dmenu, creates .xinitrc, and applies themes
-./ultimate-system/update.sh
+# recompiles dwm
+cd ~/dwm
+sudo make clean install
+# create .xinitrc to start dwm and display batery and date
+cd ~
+sudo cp ultimate-system/.xinitrc ~
+# setup system theming
+sudo cp ultimate-system/settings.ini /etc/gtk-3.0
+mkdir .config/gtk-3.0
+mkdir .icons .icons/default .config/rofi
+cp ultimate-system/config.rasi .config/rofi
+cp ultimate-system/settings.ini .config/gtk-3.0
+cp ultimate-system/index.theme .icons/default
+cp ultimate-system/.Xresources ~
+cp ultimate-system/.gtkrc-2.0 ~
 # setup with my git username and email
 git config --global user.email "trenthekman68@gmail.com"
 git config --global user.name "Trent008"
