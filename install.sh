0ff0bf7661be4d4f6671c9ec3de1d2524fd22ce7
Trent008 <trenthekman68@gmail.com>
2024-05-03 11:47:53 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 # install all my packages
-pacman -Syu
-pacstrap -K base base-devel linux linux-firmware git sof-firmware libxinerama libx11 libxft libpulse xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi\
+pacstrap -K /mntbase base-devel linux linux-firmware git sof-firmware libxinerama libx11 libxft libpulse xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi\
 > networkmanager code terminator thunar feh spotify-launcher playerctl firefox dunst blueberry xarchiver unzip efibootmgr grub\
 > adapta-gtk-theme papirus-icon-theme ttf-nerd-fonts-symbols-mono shotgun brightnessctl picom intel-ucode pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire
