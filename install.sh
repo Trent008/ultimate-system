1bcebf1669056978ecf5f9971ce683815bdffd5b
Trent008 <trenthekman68@gmail.com>
2024-05-03 11:11:54 -0500

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # install all my packages
 pacman -Syu
-pacstrap -K base base-devel linux linux-firmware libxinerama libx11 libxft xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi\
-> code terminator thunar feh spotify-launcher playerctl firefox dunst blueberry xarchiver unzip \
-> adapta-gtk-theme papirus-icon-theme ttf-nerd-fonts-symbols-mono shotgun brightnessctl picom
+pacstrap -K base base-devel linux linux-firmware git sof-firmware libxinerama libx11 libxft libpulse xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi\
+> networkmanager code terminator thunar feh spotify-launcher playerctl firefox dunst blueberry xarchiver unzip efibootmgr grub\
+> adapta-gtk-theme papirus-icon-theme ttf-nerd-fonts-symbols-mono shotgun brightnessctl picom intel-ucode pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire
