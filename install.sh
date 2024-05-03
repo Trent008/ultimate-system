9e832b0f073dcdeab523bdf4d30c5bb7b231aa27
Trent008 <trenthekman68@gmail.com>
2024-05-03 11:50:24 -0500

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# install all my packages
+#!/bin/bash
 pacstrap -K /mnt base base-devel linux linux-firmware git sof-firmware libxinerama libx11 libxft libpulse xorg-server xorg-xinit xorg-xsetroot xorg-xrandr rofi\
 > networkmanager code terminator thunar feh spotify-launcher playerctl firefox dunst blueberry xarchiver unzip efibootmgr grub\
 > adapta-gtk-theme papirus-icon-theme ttf-nerd-fonts-symbols-mono shotgun brightnessctl picom intel-ucode pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire
