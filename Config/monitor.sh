7d2845757a98acae635df4223c13181fb958ec71
Trent008 <trenthekman68@gmail.com>
2024-06-09 08:49:32 -0700

    switch to feh --bg-fill for background

--- Config/monitor.sh
+++ Config/monitor.sh
@@ -1,2 +1,2 @@
 xrandr --output eDP-1 --primary --pos 320x1440 --output HDMI-1 --auto --pos 0x0
-feh --bg-max ~/ultimate-system/Config/background.png
+feh --bg-fill ~/ultimate-system/Config/background.png
