29242ae0a0448bb354073b2e9ec0011e8c66c81e
Trent008 <trenthekman68@gmail.com>
2024-06-29 09:18:45 -0700

    make monitor config easier to use

--- Config/monitor.sh
+++ Config/monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off
+~/.screenlayout/apply-layout.sh
 feh --bg-fill ~/ultimate-system/Config/background.png
