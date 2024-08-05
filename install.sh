f599e69c2a62c0f86cfd0c5b62d17fa920c3aab6
Trent008 <trenthekman68@gmail.com>
2024-08-04 17:01:46 -0700

    fix install script

--- install.sh
+++ install.sh
@@ -43,9 +43,9 @@ yay -S --needed\
 cd ~/ultimate-system/dwm
 sudo make install
 sudo make clean
+~/ultimate-system/configure.sh
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now NetworkManager.service
 git config --global user.email "trenthekman68@gmail.com"
 git config --global user.name "Trent008"
 git config --global pull.rebase false
-./configure.sh
